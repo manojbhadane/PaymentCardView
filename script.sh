e7f3596745e7cb3dc48017666afd7eb6b2122b9b
manoj.bhadane <manojbhadane777@gmail.com>
2019-02-26 16:11:03 +0530

    minor changes

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 
-echo "some data" >> sample.txt
-git add .
-git commit -m "minor changes" 
-git push origin master
-
+for i in 1 2 3 4 5
+do
+  echo "Commit Number : $i"
+  echo "some data" >> sample.txt
+  git add .
+  git commit -m "minor changes" 
+  git push origin master
+done
